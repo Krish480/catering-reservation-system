fc0b8ec1eca231b5e08d68ae880462babe7590cc
Krishna Pandey <krishnapandey18866@gmail.com>
2025-09-15 17:45:39 +0530

    repaste code of firebase

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 const { initializeApp } = require("firebase/app");
 const { getFirestore } = require("firebase/firestore");
-
+const { getAuth } = require("firebase/auth");
 // Firebase config 
 const firebaseConfig = {
   apiKey: "AIzaSyAX_mqo76e7rTTYJDPJ1djUboGGxENvjow",
@@ -11,8 +11,9 @@ const firebaseConfig = {
   appId: "1:237268631141:web:4c10d6c99d1d4c9b53108c"
 };
 
-// Init
+// Initialize
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-module.exports = db;
+module.exports = { db, auth };
